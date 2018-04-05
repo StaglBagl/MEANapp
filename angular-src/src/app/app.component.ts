0001580b2aca95cdb00e69d7ac506c80b922c215
Eric Stagl <StaglBagl@gmail.com>
2018-04-04 21:33:37 -0500

    add /app

---
+++ angular-src/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
